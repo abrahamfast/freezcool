e9403335ae0a9917bb0117047514387be2f03f3d
abraham ghaemi <ig.ghaemi@gmail.com>
2021-06-10 20:49:46 +0430

    added rating filter view

---
+++ app/Http/Livewire/FilterRatingList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FilterRatingList extends Component
+{
+    public function render()
+    {
+        return view('livewire.filter-rating-list');
+    }
+}
---
+++ resources/views/livewire/filter-rating-list.blade.php
@@ -0,0 +1,147 @@
+<div class="filter-rating">
+    <ul class="filter-rating__list">
+        <li class="filter-rating__item">
+            <label class="filter-rating__item-label">
+                                        <span class="input-check filter-rating__item-input">
+                                            <span class="input-check__body">
+                                                <input class="input-check__input" type="checkbox">
+                                                <span class="input-check__box"></span>
+                                                <span class="input-check__icon"><svg width="9px" height="7px">
+                                                        <path d="M9,1.395L3.46,7L0,3.5L1.383,2.095L3.46,4.2L7.617,0L9,1.395Z" />
+                                                    </svg>
+                                                </span>
+                                            </span>
+                                        </span>
+                <span class="filter-rating__item-stars">
+                                            <div class="rating">
+                                                <div class="rating__body">
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star rating__star--active"></div>
+                                                </div>
+                                            </div>
+                                        </span>
+                <span class="filter-rating__item-title sr-only">
+                                            5 stars
+                                        </span>
+                <span class="filter-rating__item-counter">42</span>
+            </label>
+        </li>
+        <li class="filter-rating__item">
+            <label class="filter-rating__item-label">
+                                        <span class="input-check filter-rating__item-input">
+                                            <span class="input-check__body">
+                                                <input class="input-check__input" type="checkbox">
+                                                <span class="input-check__box"></span>
+                                                <span class="input-check__icon"><svg width="9px" height="7px">
+                                                        <path d="M9,1.395L3.46,7L0,3.5L1.383,2.095L3.46,4.2L7.617,0L9,1.395Z" />
+                                                    </svg>
+                                                </span>
+                                            </span>
+                                        </span>
+                <span class="filter-rating__item-stars">
+                                            <div class="rating">
+                                                <div class="rating__body">
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star"></div>
+                                                </div>
+                                            </div>
+                                        </span>
+                <span class="filter-rating__item-title sr-only">
+                                            4 stars
+                                        </span>
+                <span class="filter-rating__item-counter">24</span>
+            </label>
+        </li>
+        <li class="filter-rating__item">
+            <label class="filter-rating__item-label">
+                                        <span class="input-check filter-rating__item-input">
+                                            <span class="input-check__body">
+                                                <input class="input-check__input" type="checkbox">
+                                                <span class="input-check__box"></span>
+                                                <span class="input-check__icon"><svg width="9px" height="7px">
+                                                        <path d="M9,1.395L3.46,7L0,3.5L1.383,2.095L3.46,4.2L7.617,0L9,1.395Z" />
+                                                    </svg>
+                                                </span>
+                                            </span>
+                                        </span>
+                <span class="filter-rating__item-stars">
+                                            <div class="rating">
+                                                <div class="rating__body">
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star"></div>
+                                                    <div class="rating__star"></div>
+                                                </div>
+                                            </div>
+                                        </span>
+                <span class="filter-rating__item-title sr-only">
+                                            3 stars
+                                        </span>
+                <span class="filter-rating__item-counter">19</span>
+            </label>
+        </li>
+        <li class="filter-rating__item">
+            <label class="filter-rating__item-label">
+                                        <span class="input-check filter-rating__item-input">
+                                            <span class="input-check__body">
+                                                <input class="input-check__input" type="checkbox">
+                                                <span class="input-check__box"></span>
+                                                <span class="input-check__icon"><svg width="9px" height="7px">
+                                                        <path d="M9,1.395L3.46,7L0,3.5L1.383,2.095L3.46,4.2L7.617,0L9,1.395Z" />
+                                                    </svg>
+                                                </span>
+                                            </span>
+                                        </span>
+                <span class="filter-rating__item-stars">
+                                            <div class="rating">
+                                                <div class="rating__body">
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star rating__star--active"></div>
+                                                    <div class="rating__star"></div>
+                                                    <div class="rating__star"></div>
+                                                    <div class="rating__star"></div>
+                                                </div>
+                                            </div>
+                                        </span>
+                <span class="filter-rating__item-title sr-only">
+                                            2 stars
+                                        </span>
+                <span class="filter-rating__item-counter">3</span>
+            </label>
+        </li>
+        <li class="filter-rating__item">
+            <label class="filter-rating__item-label">
+                <span class="input-check filter-rating__item-input">
+                    <span class="input-check__body">
+                        <input class="input-check__input" type="checkbox">
+                        <span class="input-check__box"></span>
+                        <span class="input-check__icon"><svg width="9px" height="7px">
+                                <path d="M9,1.395L3.46,7L0,3.5L1.383,2.095L3.46,4.2L7.617,0L9,1.395Z" />
+                            </svg>
+                        </span>
+                    </span>
+                </span>
+                <span class="filter-rating__item-stars">
+                    <div class="rating">
+                        <div class="rating__body">
+                            <div class="rating__star rating__star--active"></div>
+                            <div class="rating__star"></div>
+                            <div class="rating__star"></div>
+                            <div class="rating__star"></div>
+                            <div class="rating__star"></div>
+                        </div>
+                    </div>
+                </span>
+                <span class="filter-rating__item-title sr-only">1 star</span>
+                <span class="filter-rating__item-counter">12</span>
+            </label>
+        </li>
+    </ul>
+</div>  
