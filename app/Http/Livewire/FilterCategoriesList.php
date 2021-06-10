62e48f29f7950763689d0cd51915396572f14af2
abraham ghaemi <ig.ghaemi@gmail.com>
2021-06-10 20:36:39 +0430

    filter cat in product list view

---
+++ app/Http/Livewire/FilterCategoriesList.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\ProductCategories;
+
+class FilterCategoriesList extends Component
+{
+
+    public $cates;
+
+    public function mount()
+    {
+        $this->cates = ProductCategories::where('deleted', 0)->get();
+    }
+    public function render()
+    {
+        return view('livewire.filter-categories-list');
+    }
+}
--- resources/views/livewire/product/sidebar.blade.php
+++ resources/views/livewire/product/sidebar.blade.php
@@ -18,70 +18,7 @@
                     </div>
                     <div class="widget-filters__list">
                         <div class="widget-filters__item">
-                            <div class="filter filter--opened" data-collapse-item>
-                                <button type="button" class="filter__title" data-collapse-trigger>
-                                    {{ __('global.Categories') }}
-                                    <span class="filter__arrow"><svg width="12px" height="7px">
-                                                                    <path d="M0.286,0.273 L0.286,0.273 C-0.070,0.629 -0.075,1.204 0.276,1.565 L5.516,6.993 L10.757,1.565 C11.108,1.204 11.103,0.629 10.747,0.273 L10.747,0.273 C10.385,-0.089 9.796,-0.086 9.437,0.279 L5.516,4.296 L1.596,0.279 C1.237,-0.086 0.648,-0.089 0.286,0.273 Z" />
-                                                                </svg></span>
-                                </button>
-                                <div class="filter__body" data-collapse-content>
-                                    <div class="filter__container">
-                                        <div class="filter-categories">
-                                            <ul class="filter-categories__list">
-                                                <li class="filter-categories__item filter-categories__item--current">
-                                                    <a href="">دسته بندی لایه اول</a>
-                                                    <div class="filter-categories__counter">۳۳۳</div>
-                                                </li>
-                                                <li class="filter-categories__item filter-categories__item--child">
-                                                    <a href="">دسته بندی لایه دو</a>
-                                                    <div class="filter-categories__counter">۱۲</div>
-                                                </li>
-                                                <li class="filter-categories__item filter-categories__item--child">
-                                                    <a href="">دسته بندی لایه دو</a>
-                                                    <div class="filter-categories__counter">۲۳۲</div>
-                                                </li>
-                                                <li class="filter-categories__item filter-categories__item--child">
-                                                    <a href="">دسته بندی لایه دو</a>
-                                                    <div class="filter-categories__counter">30</div>
-                                                </li>
-                                                <li class="filter-categories__item filter-categories__item--current">
-                                                    <a href="">دسته بندی لایه اول</a>
-                                                    <div class="filter-categories__counter">۷</div>
-                                                </li>
-                                                <li class="filter-categories__item filter-categories__item--child">
-                                                    <a href="">دسته بندی لایه دو</a>
-                                                    <div class="filter-categories__counter">۵</div>
-                                                </li>
-                                                <li class="filter-categories__item filter-categories__item--current">
-                                                    <a href="">دسته بندی لایه اول</a>
-                                                    <div class="filter-categories__counter">۲</div>
-                                                </li>
-                                                <li class="filter-categories__item filter-categories__item--child">
-                                                    <a href="">دسته بندی لایه دو</a>
-                                                    <div class="filter-categories__counter">۹</div>
-                                                </li>
-                                                <li class="filter-categories__item filter-categories__item--child">
-                                                    <a href="">دسته بندی لایه دو</a>
-                                                    <div class="filter-categories__counter">۴</div>
-                                                </li>
-                                                <li class="filter-categories__item filter-categories__item--child">
-                                                    <a href="">دسته بندی لایه دو</a>
-                                                    <div class="filter-categories__counter">۰</div>
-                                                </li>
-                                                <li class="filter-categories__item filter-categories__item--child">
-                                                    <a href="">دسته بندی لایه دو</a>
-                                                    <div class="filter-categories__counter">۱۴</div>
-                                                </li>
-                                                <li class="filter-categories__item filter-categories__item--child">
-                                                    <a href="">دسته بندی لایه دو</a>
-                                                    <div class="filter-categories__counter">۸</div>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+   <livewire:filter-categories-list />
                         </div>
                         <div class="widget-filters__item">
                             <div class="filter filter--opened" data-collapse-item>
