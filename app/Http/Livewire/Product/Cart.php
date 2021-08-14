554717ab4b762aa60384acc62b31eded72741dc0
Abraham ghaemi <ig.ghaemi@gmail.com>
2021-08-14 10:18:45 +0430

    refactoring

--- app/Http/Livewire/Product/Cart.php
+++ app/Http/Livewire/Product/Cart.php
@@ -8,6 +8,15 @@ class Cart extends Component
 {
     public $product;
     public $wishlist = [];
+    public $compare_list;
+
+    public function mount()
+    {
+        $compare = session()->get('compare');
+        $this->compare_list = collect($compare)->map(function($iten){
+            return $iten->id;
+        });
+    }
 
     public function render()
     {
--- resources/views/livewire/product/cart.blade.php
+++ resources/views/livewire/product/cart.blade.php
@@ -119,7 +119,8 @@
                 </svg>
                 <span>{{ __('global.Add to wishlist') }}</span>
             </a>
-            <a  href="{{ route('compare.add', $product->id) }}" class="product-card__compare" type="button">
+
+            <a  href="{{ route('compare.add', $product->id) }}" @if(in_array($product->id, $compare_list->toArray()))style="fill: #30a24a; background-color: #33a7443d" @endif class="product-card__compare" type="button">
                 <svg width="16" height="16">
                     <path d="M9,15H7c-0.6,0-1-0.4-1-1V2c0-0.6,0.4-1,1-1h2c0.6,0,1,0.4,1,1v12C10,14.6,9.6,15,9,15z" />
                     <path d="M1,9h2c0.6,0,1,0.4,1,1v4c0,0.6-0.4,1-1,1H1c-0.6,0-1-0.4-1-1v-4C0,9.4,0.4,9,1,9z" />
